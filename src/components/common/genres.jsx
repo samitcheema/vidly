3abeb767ad7fc9d47040c761c9e1dfdaea638390
usamatcheema <usamatcheema@gmail.com>
2020-01-09 01:04:00 -0500

    implemented list selection

--- src/components/common/genres.jsx
+++ src/components/common/genres.jsx
@@ -1,16 +1,34 @@
 import React, { Component } from "react";
 
 const Genres = props => {
-  const { items, valueProperty, textProperty } = props;
+  const {
+    items,
+    valueProperty,
+    textProperty,
+    onItemSelect,
+    selectedItem
+  } = props;
   return (
     <ul class="list-group">
       {items.map(item => (
-        <li key={item[valueProperty]} className="list-group-item">
+        <li
+          onClick={() => onItemSelect(item)}
+          key={item[valueProperty]}
+          className={
+            item === selectedItem ? "list-group-item active" : "list-group-item"
+          }
+          style={{ cursor: "Pointer" }}
+        >
           {item[textProperty]}
         </li>
       ))}
     </ul>
   );
+};
+
+Genres.defaultProps = {
+  textProperty: "name",
+  valueProperty: "_id"
 };
 
 export default Genres;
