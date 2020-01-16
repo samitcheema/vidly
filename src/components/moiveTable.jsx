47aeab26573b2b3bfdf2eeb3d2bfce5ec73e6770
usamatcheema <usamatcheema@gmail.com>
2020-01-16 04:37:39 -0500

    link to movie form added

--- src/components/moiveTable.jsx
+++ src/components/moiveTable.jsx
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import Favorite from "./common/favorite";
 import Table from "./common/table";
 
 class MovieTable extends Component {
   columns = [
-    { path: "title", label: "Title" },
+    {
+      path: "title",
+      label: "Title",
+      content: movie => <Link to={`/movies/${movie._id}`}>{movie.title}</Link>
+    },
     { path: "genre.name", label: "Genre" },
     { path: "numberInStock", label: "Stock" },
     { path: "dailyRentalRate", label: "Rate" },
