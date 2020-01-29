f7fc410800a8415a873c1d51462af02849d010e8
usamatcheema <usamatcheema@gmail.com>
2020-01-29 17:06:18 -0500

    cleaned

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -2,15 +2,25 @@ import http from "./httpServices";
 
 const apiEndpoint = "http://localhost:3900/api/movies";
 
+function movieUrl(id) {
+  return `${apiEndpoint}/${id}`;
+}
+
 export function getMovies() {
   return http.get(apiEndpoint);
 }
 
 export function getMovie(movieId) {
-  return http.get(apiEndpoint + "/" + movieId);
+  return http.get(movieUrl(movieId));
 }
 
-export function saveMovie(movie) {}
+export function saveMovie(movie) {
+  if (movie._id) {
+    const body = { ...movie };
+    delete body._id;
+    http.put(movieUrl(movie._id), body);
+  }
+}
 
 export function deleteMovie(movieId) {
   http.delete(apiEndpoint + "/" + movieId);
